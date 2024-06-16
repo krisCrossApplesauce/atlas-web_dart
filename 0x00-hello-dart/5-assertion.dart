cf0caa7809dcb3e6b578517d4feba2c5fa0b94d2
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-16 13:24:09 -0500

    5: I cannot believe I ruled out tertiary operators bc my brain equated them to being if/else statements and therefore something we're not supposed to use

---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  /*
+  Write Your code below 
+  */
+  print(${args[3] >= 80 ? 'You passed' : 'Uncaught Error: Assertion failed: The score must be bigger or equal to 80'});
+}
