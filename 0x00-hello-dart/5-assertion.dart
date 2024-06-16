831f93027e933c6902db062a4a0acea8b15d7f92
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-16 13:38:01 -0500

    5: I wish someone would help me figure out how to install Dart so I could test it myself instead of having to push it and run it through the checker repeatedly

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,4 +1,4 @@
 void main(List<String> args) {
-  str output = int.parse(args[3]) >= 80 ? 'You passed' : 'Uncaught Error: Assertion failed: The score must be bigger or equal to 80';
+  String output = int.parse(args[3]) >= 80 ? 'You passed' : 'Uncaught Error: Assertion failed: The score must be bigger or equal to 80';
   print(output);
 }
