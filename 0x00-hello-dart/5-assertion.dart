68328ea3b40d2aad6c0a07fd62a2331c2619c8c6
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-16 13:31:20 -0500

    5: this confuses me, I'm not sure how to get the error part to work, I also think that the checker/example use different wording than the task description which is very weird

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,3 +1,3 @@
 void main(List<String> args) {
-  print(${args[3] >= 80 ? 'You passed' : 'Uncaught Error: Assertion failed: The score must be bigger or equal to 80'});
+  print('${args[3] >= 80 ? 'You passed' : 'Uncaught Error: Assertion failed: The score must be bigger or equal to 80'}');
 }
