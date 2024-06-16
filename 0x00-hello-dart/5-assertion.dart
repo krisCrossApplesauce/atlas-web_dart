4e4bf7424e4335d88a7be877eeda0ebf39676772
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-16 13:24:52 -0500

    5: ternary, not tertiary lol

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,6 +1,3 @@
 void main(List<String> args) {
-  /*
-  Write Your code below 
-  */
   print(${args[3] >= 80 ? 'You passed' : 'Uncaught Error: Assertion failed: The score must be bigger or equal to 80'});
 }
