47ff9f01342c3f3825ee3866b7443cdbccfaf303
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-17 13:48:56 -0500

    9

---
+++ 0x01-dart-function/9-palindrome.dart
@@ -0,0 +1,9 @@
+bool isPalindrome(String s) {
+  if (s.length < 3) return false;
+  for (var i = 0; i < (s.length / 2); i++) {
+    if (s[i] != s[s.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
