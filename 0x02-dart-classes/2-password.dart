bb8aee4b9f7f06a29ca676bea9b6c3a69637b04c
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-18 14:16:14 -0500

    3

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -2,11 +2,6 @@ class Password {
   String? password;
 
   bool? isValid() {
-    // assert(password!.length >= 8);
-    // assert(password!.length <= 16);
-    // assert(password!.contains(RegExp(r'[A-Z]')));
-    // assert(password!.contains(RegExp(r'[a-z]')));
-    // assert(password!.contains(RegExp(r'[0-9]')));
     if (password!.length >= 8
       && password!.length <= 16
       && password!.contains(RegExp(r'[A-Z]'))
