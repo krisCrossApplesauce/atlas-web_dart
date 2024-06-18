21e84f465de39e4830d04601f90ecbbf1ab90f8e
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-18 14:27:13 -0500

    4: I want the checker to fail so that I can actually see how it's using the getter and setter

--- 0x02-dart-classes/4-mutables.dart
+++ 0x02-dart-classes/4-mutables.dart
@@ -1,17 +1,17 @@
 class Password {
   String? _password;
 
-  Password({required String password}) {
-    _password = password;
-  }
+  // Password({required String password}) {
+  //   _password = password;
+  // }
 
-  String? get password {
-    return _password;
-  }
+  // String? get password {
+  //   return _password;
+  // }
 
-  set password(String? password) {
-    _password = password;
-  }
+  // set password(String? password) {
+  //   _password = password;
+  // }
 
   bool? isValid() {
     if (_password!.length >= 8
@@ -20,11 +20,11 @@ class Password {
       && _password!.contains(RegExp(r'[a-z]'))
       && _password!.contains(RegExp(r'[0-9]'))
     ) return true;
-    return false;
+    // return false;
   }
 
-  @override
-  String toString() {
-    return 'Your Password is: ' + _password!;
-  }
+  // @override
+  // String toString() {
+  //   return 'Your Password is: ' + _password!;
+  // }
 }
