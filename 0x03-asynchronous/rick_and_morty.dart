113f423cb70159913cfab53f212518e595b89fc7
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-22 14:22:32 -0500

    5: ok I just needed to continue reading the thing I was reading on how to do this and I needed to throw an Exception if the statusCode isn't correct :/

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -4,6 +4,9 @@ import 'dart:convert';
 Future<void> printRmCharacters() async {
   try {
     http.Response response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character/'));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load data');
+    }
     List rmCharacters = jsonDecode(response.body)['results'];
     for (int i = 0; i < rmCharacters.length; i++) {
       print(rmCharacters[i]['name']);
