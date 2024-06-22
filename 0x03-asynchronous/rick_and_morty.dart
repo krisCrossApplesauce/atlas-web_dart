7e320f3611394e7392d5d752b27e80ff3ebb78c4
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-22 14:25:22 -0500

    5: ... it didn't like that I had the type for response as http.response opposed to just setting it as final ._.

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,13 +3,14 @@ import 'dart:convert';
 
 Future<void> printRmCharacters() async {
   try {
-    http.Response response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character/'));
-    if (response.statusCode != 200) {
+    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character/'));
+    if (response.statusCode == 200) {
+      List rmCharacters = jsonDecode(response.body)['results'];
+      for (int i = 0; i < rmCharacters.length; i++) {
+        print(rmCharacters[i]['name']);
+      }
+    } else {
       throw Exception('Failed to load data');
-    }
-    List rmCharacters = jsonDecode(response.body)['results'];
-    for (int i = 0; i < rmCharacters.length; i++) {
-      print(rmCharacters[i]['name']);
     }
   } catch (err) {
     print('error caught: $err');
