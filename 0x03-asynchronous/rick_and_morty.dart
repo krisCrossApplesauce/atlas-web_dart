bfab36dd905c5248b4117aeb2779d3303f958c93
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-22 14:03:29 -0500

    5: ok, I think it doesn't want anything returned, I think it only wants it to print stuff, changing the output type to Future<void> seems to work. Still don't understand what the 'Get dependencies' check is wanting though

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,17 +1,17 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<String> printRmCharacters() async {
+Future<void> printRmCharacters() async {
   try {
     http.Response RmCharactersResponse = await http.get(Uri.parse('https://rickandmortyapi.com/api/character/'));
     List RmCharacters = jsonDecode(RmCharactersResponse.body)['results'];
-    String RmCharacterNames = '';
+    // String RmCharacterNames = '';
     for (int i = 0; i < RmCharacters.length; i++) {
-      // print(RmCharacters[i]['name']);
-      RmCharacterNames += RmCharacters[i]['name'];
+      print(RmCharacters[i]['name']);
+      // RmCharacterNames += RmCharacters[i]['name'];
     }
-    return RmCharacterNames;
+    // return RmCharacterNames;
   } catch (err) {
-    return 'error caught: $err';
+    print('error caught: $err');
   }
 }
