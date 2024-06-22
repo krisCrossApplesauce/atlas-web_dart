a3b69248cba9806d630c2e05c00448b3ebf51afc
kris_cross_applesauce <karisrr2203@gmail.com>
2024-06-22 13:58:45 -0500

    5? it says to return 'error caught: <error>' if an error occurs but says to print the character names and doesn't say to return anything. Also the main file doesn't print the results of the function and just runs the function. The function also doesn't let me set the output as Future<String?> as it results in a warning and it doesn't allow the type to be void either I think because it's being run from a separate function, but it could be bc I didn't have it as Future<void>... But that still wouldn't work because it'd be returning something if there's an error :/

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,6 +1,17 @@
-void printRmCharacters() {
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<String> printRmCharacters() async {
   try {
+    http.Response RmCharactersResponse = await http.get(Uri.parse('https://rickandmortyapi.com/api/character/'));
+    List RmCharacters = jsonDecode(RmCharactersResponse.body)['results'];
+    String RmCharacterNames = '';
+    for (int i = 0; i < RmCharacters.length; i++) {
+      print(RmCharacters[i]['name']);
+      RmCharacterNames += RmCharacters[i]['name'];
+    }
+    return RmCharacterNames;
   } catch (err) {
-    print('error caught: $err');
+    return 'error caught: $err';
   }
 }
